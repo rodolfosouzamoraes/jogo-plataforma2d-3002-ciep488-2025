afcc426eda3bda159eb8e7a3201ab6d8ea65b680
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2025-02-25 07:44:48 -0300

    Inicio do level

---
+++ Assets/InicioDoLevel.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class InicioDoLevel : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject posicaoPlayer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindFirstObjectByType<MovimentarPlayer>().gameObject;
+        player.transform.position = posicaoPlayer.transform.position;
+    }
+}
